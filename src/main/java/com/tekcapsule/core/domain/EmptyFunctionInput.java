218e0a31e1816abdc8c220294c86848aee4cbe6c
Linjith  Kunnon <linjithkunnon@Linjiths-MacBook-Pro.local>
2021-10-22 20:32:47 +0530

    refactoring

---
+++ src/main/java/com/tekcapsule/core/domain/EmptyFunctionInput.java
@@ -0,0 +1,11 @@
+package com.tekcapsule.core.domain;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode
+@Builder
+public class EmptyFunctionInput {
+}
