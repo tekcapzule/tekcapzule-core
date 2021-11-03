7f4ab05981362b6df55c56333061ff434ccf085a
Linjith  Kunnon <linjithkunnon@Linjiths-MacBook-Pro.local>
2021-11-03 14:38:35 +0530

    fixed the dynamodb config

--- src/main/java/com/tekcapsule/core/config/DynamoDBConfig.java
+++ src/main/java/com/tekcapsule/core/config/DynamoDBConfig.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverterFactory;
 import com.amazonaws.services.s3.model.Region;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -23,11 +24,21 @@ public class DynamoDBConfig {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withRegion(String.valueOf(Region.valueOf(cloudRegion)))
                 .build();
+
+
         return new DynamoDBMapper(client, DynamoDBMapperConfig.DEFAULT);
     }
     @Bean
-    public DynamoDBMapperConfig.TableNameOverride tableNameOverrider() {
+    public DynamoDBMapperConfig.TableNameOverride getTableNameOverride() {
         String prefix = applicationEnvironment;
         return DynamoDBMapperConfig.TableNameOverride.withTableNamePrefix(prefix);
+    }
+
+    @Bean
+    public DynamoDBMapperConfig getDynamoDBMapperConfig(DynamoDBMapperConfig.TableNameOverride tableNameOverride) {
+        DynamoDBMapperConfig.Builder builder = new DynamoDBMapperConfig.Builder();
+        builder.setTableNameOverride(tableNameOverride);
+        builder.setTypeConverterFactory(DynamoDBTypeConverterFactory.standard());
+        return builder.build();
     }
 }
