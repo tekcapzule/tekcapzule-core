5bacaaf599b565dfd90281fed8a42ea4b4d79014
Linjith  Kunnon <linjithkunnon@Linjiths-MacBook-Pro.local>
2021-11-05 11:20:35 +0530

    added utility function for populating headers

--- src/main/java/com/tekcapsule/core/utils/HeaderUtil.java
+++ src/main/java/com/tekcapsule/core/utils/HeaderUtil.java
@@ -4,10 +4,22 @@ import com.tekcapsule.core.domain.Origin;
 import com.tekcapsule.core.domain.SourceSystem;
 import org.springframework.messaging.MessageHeaders;
 
+import java.util.Map;
+
 public class HeaderUtil {
 
     public static final String USER_ID = "x-userId";
     public static final String CHANNEL = "x-channel";
+
+    public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+
+    public static final String ALLOW_METHODS = "POST";
+    public static final String ALLOW_CREDENTIALS = "true";
+    public static final String ALLOW_ORIGIN = "*";
+    public static final String ALLOW_HEADERS = "Content-Type";
 
     public static Origin buildOriginFromHeaders(MessageHeaders headers) {
         /*SourceSystem sourceSystem = SourceSystem.valueOf(headers.get(CHANNEL).toString());
@@ -25,4 +37,15 @@ public class HeaderUtil {
                 .build();
         return origin;
     }
+
+    public static Map<String, Object> populateCorsHeaders(Map<String, Object> responseHeader) {
+
+        responseHeader.put(ACCESS_CONTROL_ALLOW_CREDENTIALS, ALLOW_CREDENTIALS);
+        responseHeader.put(ACCESS_CONTROL_ALLOW_HEADERS, ALLOW_HEADERS);
+        responseHeader.put(ACCESS_CONTROL_ALLOW_METHODS, ALLOW_METHODS);
+        responseHeader.put(ACCESS_CONTROL_ALLOW_ORIGIN, ALLOW_ORIGIN);
+
+        return responseHeader;
+    }
+
 }
