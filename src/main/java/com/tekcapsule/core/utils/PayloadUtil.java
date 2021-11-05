ea0e2a728fb24cfc752d0e2a6e771667c77ad5c6
Linjith  Kunnon <linjithkunnon@Linjiths-MacBook-Pro.local>
2021-11-05 13:14:10 +0530

    refactoring

--- src/main/java/com/tekcapsule/core/utils/PayloadUtil.java
+++ src/main/java/com/tekcapsule/core/utils/PayloadUtil.java
@@ -10,17 +10,17 @@ public class PayloadUtil {
     public static final String SUCCESS_MESSAGE = "Congratulations! Your request has made its way!";
     public static final String SUCCESS_CODE = "200";
 
-    public static Map<String, Object> composePayload(Map<String, Object> responseHeader, Outcome outcome) {
+    public static Map<String, Object> composePayload(Outcome outcome) {
 
         Map<String, Object> payload = new HashMap();
 
         if (outcome == Outcome.SUCCESS) {
-            responseHeader.put("code", SUCCESS_CODE);
-            responseHeader.put("message", SUCCESS_MESSAGE);
+            payload.put("code", SUCCESS_CODE);
+            payload.put("message", SUCCESS_MESSAGE);
         } else {
-            responseHeader.put("code", ERROR_CODE);
-            responseHeader.put("message", ERROR_MESSAGE);
+            payload.put("code", ERROR_CODE);
+            payload.put("message", ERROR_MESSAGE);
         }
-        return responseHeader;
+        return payload;
     }
 }
